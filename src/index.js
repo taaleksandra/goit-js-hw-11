8711a514d0512ce14e6b06d7046f5231766f31ed
taaleksandra <olatarnas@gmail.com>
2022-12-12 23:08:09 +0100

    paginacja

--- src/index.js
+++ src/index.js
@@ -10,9 +10,13 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 const formSearch = document.querySelector('#search-form');
 const inputSearch = document.querySelector('input[name="searchQuery"]');
 const btnSearch = document.querySelector('button');
+const btnLoad = document.querySelector('.load-more');
 const galerry = document.querySelector('.gallery');
 
 let page = 1;
+let limit = 40;
+btnLoad.classList.add('is-invisible');
+btnSearch.disabled = true;
 
 // funkcja z zapytaniem HTTP wykorzystująca Axios i skłądnię async/await
 const fetchImg = async q => {
@@ -30,13 +34,18 @@ const fetchImg = async q => {
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: true,
+        per_page: limit,
+        page,
       },
     });
 
     // console.log(response);
 
+    btnLoad.classList.add('is-invisible');
+
     const totalHits = response.data.totalHits;
     const total = response.data.total;
+    const totalPages = Math.ceil(totalHits / 40);
 
     if (total === 0) {
       Notiflix.Notify.init({
@@ -51,6 +60,10 @@ const fetchImg = async q => {
       `Dla podanej frazy (${trimQ}) wyszukano ${total} dopasowań (dla bezpłatnego konta: ${totalHits}).`
     );
 
+    if (totalHits > 40) {
+      btnLoad.classList.remove('is-invisible');
+    }
+
     return response.data.hits;
   } catch (error) {
     console.error(error);
@@ -59,6 +72,7 @@ const fetchImg = async q => {
 
 // funkcja tworząca galerię wyszukanych obrazów dla wpisanej frazy
 const drawGallery = hits => {
+  console.log(hits);
   const galleryArray = hits.map(
     ({ webformatURL, tags, likes, views, comments, downloads }) => {
       const imgCard = document.createElement('div');
@@ -70,16 +84,20 @@ const drawGallery = hits => {
   );
   galerry.innerHTML = '';
   galerry.append(...galleryArray);
+  // btnLoad.classList.remove('is-invisible');
 };
 
 // funkcja do obsługi pola tekstowego
 const inputHandler = ev => {
   ev.preventDefault();
   const searchingImg = inputSearch.value;
+  page = 1;
 
   fetchImg(searchingImg)
     .then(hits => {
       drawGallery(hits);
+      console.log(`Wczytana strona: ${page}`);
+      page += 1;
     })
     .catch(err => {
       galerry.innerHTML = '';
@@ -87,5 +105,37 @@ const inputHandler = ev => {
     });
 };
 
+// funckja load more
+const loadMore = () => {
+  const searchingImg = inputSearch.value;
+  fetchImg(searchingImg)
+    .then(hits => {
+      drawGallery(hits);
+      console.log(`Wczytana strona: ${page}`);
+
+      // if (page > totalHits) {
+
+      // }
+
+      page += 1;
+    })
+    .catch(err => {
+      galerry.innerHTML = '';
+      console.error(err);
+    });
+};
+
+// funkcja udostępniająca przycisk search
+inputSearch.addEventListener('input', () => {
+  if (inputSearch.value !== '') {
+    btnSearch.disabled = false;
+  } else {
+    btnSearch.disabled = true;
+  }
+});
+
 // obsługa zdarzenia po wpisaniu danych
 formSearch.addEventListener('submit', inputHandler);
+
+// obługa zdarzenia load more
+btnLoad.addEventListener('click', loadMore);
